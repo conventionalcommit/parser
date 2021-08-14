77cb26d7364727efe740ba32f3e08cc8210c0f8e
muthukrishnan24 <muthukrishnanofficial@gmail.com>
2021-08-14 11:54:13 +0530

    refactor: move Commit to separate file

---
+++ commit.go
@@ -0,0 +1,36 @@
+package parser
+
+// Commit represents a commit that adheres to the conventional commits specification
+type Commit struct {
+	Header Header
+	Body   string
+	Footer Footer
+
+	BreakingChange bool
+
+	FullCommit string
+}
+
+// Header represents Header in commit message
+type Header struct {
+	Type        string
+	Scope       string
+	Description string
+	FullHeader  string
+}
+
+// Footer represents Footer in commit message
+type Footer struct {
+	Notes      []FooterNote
+	FullFooter string
+}
+
+// FooterNote represents one footer note in Footer
+type FooterNote struct {
+	Token string
+	Value string
+}
+
+func newFooterNote(token, value string) FooterNote {
+	return FooterNote{Token: token, Value: value}
+}
--- parser.go
+++ parser.go
@@ -13,41 +13,6 @@ var (
 	errNoNewLine = errors.New("commit description not followed by an empty line")
 )
 
-// Commit represents a commit that adheres to the conventional commits specification
-type Commit struct {
-	Header Header
-	Body   string
-	Footer Footer
-
-	BreakingChange bool
-
-	FullCommit string
-}
-
-// Header represents Header in commit message
-type Header struct {
-	Type        string
-	Scope       string
-	Description string
-	FullHeader  string
-}
-
-// Footer represents Footer in commit message
-type Footer struct {
-	Notes      []FooterNote
-	FullFooter string
-}
-
-// FooterNote represents one footer note in Footer
-type FooterNote struct {
-	Token string
-	Value string
-}
-
-func newFooterNote(token, value string) FooterNote {
-	return FooterNote{Token: token, Value: value}
-}
-
 // Parse attempts to parse a commit message to a conventional commit
 func Parse(message string) (*Commit, error) {
 	message = strings.TrimRight(message, "\n\t ")
