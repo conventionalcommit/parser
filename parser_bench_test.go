deee0e15a3d57c054d4e6c250281d33f9067c6b7
muthukrishnan24 <muthukrishnanofficial@gmail.com>
2021-09-26 10:28:17 +0530

    chore: add benchmark results

--- parser_bench_test.go
+++ parser_bench_test.go
@@ -20,8 +20,14 @@ By: John Doe`
 // regex Compile once
 // BenchmarkParser-4   	  179227	      6531 ns/op	    1478 B/op	      23 allocs/op
 
-// header regex cleaned
+// header regex clean
 // BenchmarkParser-4   	  206452	      5199 ns/op	    1414 B/op	      23 allocs/op
+
+// after deps cleanup
+// BenchmarkParser-4   	  228248	      5215 ns/op	    1414 B/op	      23 allocs/op
+
+// with fmt.Sprint removed
+// BenchmarkParser-4   	  229952	      4875 ns/op	    1365 B/op	      21 allocs/op
 
 func BenchmarkParser(b *testing.B) {
 	for i := 0; i < b.N; i++ {
